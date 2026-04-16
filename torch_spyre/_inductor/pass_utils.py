395e798495cf8ca31522720a979ca4c232ae66b7
Vivek Mankar <Vivek.Mankar@ibm.com>
2026-04-16 20:50:21 +0530

    move dynamic shape tests to tests/dynamic_shapes/

--- torch_spyre/_inductor/pass_utils.py
+++ torch_spyre/_inductor/pass_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import NamedTuple, Union
+from typing import Callable, NamedTuple, TypeVar, Union
 
 
 import sympy
